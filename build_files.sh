1dba4aa86a4828729f5577f00cd5471408e58e06
bali singh <balisc04@gmail.com>
2023-08-17 17:20:48 +0530

    edit build files

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 echo " BUILD START"
 pip install -r requirements.txt
 pip install django
+pip install sqlite
+pip install pywin32
 python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD END" 
