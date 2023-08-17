2a66fd6f49891f3c1637c43ec5ad14e51b5a93e5
bali singh <balisc04@gmail.com>
2023-08-17 17:56:28 +0530

    c

--- gamerszone/settings.py
+++ gamerszone/settings.py
@@ -73,12 +73,12 @@ WSGI_APPLICATION = 'gamerszone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
@@ -118,8 +118,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 import os
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
