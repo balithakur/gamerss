eeaf6e3c18d251ccd2db3ef293ac3accf32ae020
bali singh <balisc04@gmail.com>
2023-08-17 17:08:49 +0530

    change settings

--- gamerszone/settings.py
+++ gamerszone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mehmp!=hdf9k_e=4f=nq!k6*p07s*+3(zie$oamct%eh^d6h%l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
