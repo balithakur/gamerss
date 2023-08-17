f721aa02b85f8bef2f78df24b370cdb08d7a89e4
bali singh <balisc04@gmail.com>
2023-08-17 17:29:23 +0530

    modify requirement file

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 echo " BUILD START"
 pip install -r requirements.txt
 pip install django
-pip install sqlite
+pip install pysqlite3
 pip install pywin32
 python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD END" 
